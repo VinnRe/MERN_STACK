1a890710e83138f6c739affcc08a5b9433eb881e
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-03-28 22:38:57 +0800

    Update initialized files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-import Message from './Message'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import NavBar from "./components/NavBar"
 
 function App() {
-  return <div><Message></Message></div>
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
