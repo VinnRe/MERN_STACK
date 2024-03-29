a09bcd19becd2ba8e7ea7c80522fae2a57df95b5
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-03-30 00:22:58 +0800

    Add error handler and style for form

--- frontend/src/components/WorkoutForm.tsx
+++ frontend/src/components/WorkoutForm.tsx
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState<string | null>(null)
+    const [emptyFields, setEmptyFields] = useState<string[]>([])
     
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -25,12 +26,14 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log("New Workout Added")
             dispatch({type: 'CREATE_WORKOUT', payload: json})
         }
@@ -45,18 +48,21 @@ const WorkoutForm = () => {
                 type="text" 
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
             <label>Load (KG):</label>
             <input 
                 type="number" 
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
             <label>Repititions:</label>
             <input 
                 type="number" 
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
