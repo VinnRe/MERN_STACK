52ceb09123afeb797ddc67a611bd8849f0696adf
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-03-29 15:29:34 +0800

    Add displaying and fetching of data

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+interface Workout {
+    _id: string
+    title: string
+    load: number
+    reps: number
+}
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState<Workout[] | null>(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async() => {
+            const response = await fetch('/api/workouts')
+            console.log('Fetch request:', response.url)
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
     return (
-        <div className="Home">
-            <h2>Home</h2>
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
