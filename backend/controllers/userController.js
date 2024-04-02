9f25feaf449f5880bd3af9f23851670ebd9d2bcc
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-04-02 21:20:31 +0800

    Add pasword hashing logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,15 @@ const loginUser = async (req, res) => {
 
 // Signup user
 const signupUser = async (req, res) => {
-    res.json({msg: 'Signup User'})
+    const { email, password } = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.statues(400).json({ error: error.message})
+    }
 }
 
 
