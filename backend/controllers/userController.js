682cb169dd5e26e47fddb2a3b63707583392ca37
VinnRe <22-03804@g.batstate-u.edu.ph>
2024-04-02 22:06:58 +0800

    Add JWT and Login/Signup logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // Login user
 const loginUser = async (req, res) => {
-    res.json({msg: 'Login User'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({ error: error.message})
+    }
 }
 
 
@@ -13,7 +29,10 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({ error: error.message})
     }
